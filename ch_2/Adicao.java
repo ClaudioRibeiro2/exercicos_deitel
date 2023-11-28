aecb86ae7ccb93b4bf0d49ce155371e346f9208c
ClaudioRibeiro2 <claudio_ribeiro2@estudante.sc.senai.br>
2023-11-28 11:51:38 -0300

    First commit! Reading the Chapter 2.

---
+++ ch_2/Adicao.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Adicao {
+    // Main method
+    public static void main(String[] args) {
+        // Attributes
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2, sum; // Bad programming practice
+			
+        System.out.println("Hello! Please enter the first int: ");
+        num1 = scanner.nextInt();
+
+        System.out.println("Enter the second int: ");
+        num2 = scanner.nextInt();
+
+        sum = num1 + num2;
+
+        System.out.printf("The sum is %d%n", sum);
+        scanner.close();
+    } // End of the main method
+} // End of the class Adicao
